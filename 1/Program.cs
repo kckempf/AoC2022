88151e4e0588892ba75e9f40b39eb62f23f48a63
EC2 Default User <ec2-user@ip-172-31-75-111.ec2.internal>
2022-12-01 15:39:54 +0000

    Day 1 Solution

---
+++ 1/Program.cs
@@ -0,0 +1,40 @@
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var input = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"));
+
+var max = 0;
+var second = 0;
+var third = 0;
+var temp = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    var current = input[i];
+    if (String.IsNullOrEmpty(current))
+    {
+        if (temp > max)
+        {
+            third = second;
+            second = max;
+            max = temp;
+        }
+        else if (temp > second)
+        {
+            third = second;
+            second = temp;
+        }
+        else if (temp > third)
+        {
+            third = temp;
+        }
+        temp = 0;
+    }
+    else
+    {
+        int.TryParse(current, out int number);
+        temp += number;
+    }
+}
+max = Math.Max(max, temp);
+Console.WriteLine($"Answer 1: {max}");
+Console.WriteLine($"Answer 2: {max + second + third}");
